979dd5fce19d17a7012b7652b341472167d813c7
Yves Müller <yves.mueller@liqd.de>
2017-10-30 13:10:40 +0100

    Add aws specific settings file

---
+++ euth_wagtail/settings/aws.py
@@ -0,0 +1,20 @@
+from os import environ
+
+from .base import *
+
+DEBUG = False
+
+try:
+    from .local import *
+except ImportError:
+    pass
+
+INSTALLED_APPS += [
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
+]
+
+ALLOWED_HOSTS = [
+    'dev-env.igharuux97.eu-central-1.elasticbeanstalk.com',
+    'localhost',
+]
